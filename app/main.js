474d5b8727fba8548f3d9c72bfe342c5266cc9ac
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-04-23 17:09:23 +0200

    Move to app subfolder, drop terminal, add icon.
    - Ooops, only one commit there :(

--- app/main.js
+++ app/main.js
@@ -37,27 +37,26 @@ var INDEX_TIME_HEIZUNG_RUECKLAUF_WENIGER = 75;
  * Temperature unit.
  * @const
  */
-var UNIT_TEMPERATURE_CELSIUS = '°C';
+var UNIT_TEMPERATURE_CELSIUS = "°C";
 
 var tcpClient;
 var isRequestStatus = false;
+
+var dataView;
+var statusView;
 
 /**
  * Shows and hides the help panel
  */
 function toggleHelp() {
-  document.querySelector('.help').classList.toggle('hidden');
-  document.body.classList.toggle('dim');
+  document.querySelector(".help").classList.toggle("hidden");
+  document.body.classList.toggle("dim");
 }
 
 (function () {
 
-  // Create and init the terminal
-  var term = new Terminal('container');
-  term.initFS(false, 1024 * 1024);
-
   // Capture key presses
-  document.body.addEventListener('keydown', function (e) {
+  document.body.addEventListener("keydown", function (e) {
     if (e.keyCode == 27) { // Esc
       toggleHelp();
       e.stopPropagation();
@@ -65,20 +64,18 @@ function toggleHelp() {
     }
   }, false);
 
-  term.output('Press Esc for options.<br/>');
-
   // Connect to WP by default.
-  var host = 'waermepumpe';
+  var host = "waermepumpe";
   var port = 8888;
-  connect(host, port);
+  // connect(host, port);
 
   // connect button
-  var connectButton = document.getElementById('connect');
-  connectButton.addEventListener('click', function () {
+  var connectButton = document.getElementById("connect");
+  connectButton.addEventListener("click", function () {
 
     // Disconnect from previous socket.
-    var host = document.getElementById('host').value;
-    var port = parseInt(document.getElementById('port').value, 10);
+    var host = document.getElementById("host").value;
+    var port = parseInt(document.getElementById("port").value, 10);
     disconnect();
     connect(host, port);
     toggleHelp();
@@ -86,15 +83,15 @@ function toggleHelp() {
   });
 
   // disconnect button
-  var disconnectButton = document.getElementById('disconnect');
-  disconnectButton.addEventListener('click', function () {
+  var disconnectButton = document.getElementById("disconnect");
+  disconnectButton.addEventListener("click", function () {
     disconnect();
     toggleHelp();
   });
 
   // request status toggle button
-  var requestStatusButton = document.getElementById('requestStatus');
-  requestStatusButton.addEventListener('click', function () {
+  var requestStatusButton = document.getElementById("requestStatus");
+  requestStatusButton.addEventListener("click", function () {
     if (isRequestStatus) {
       isRequestStatus = false;
     } else {
@@ -102,6 +99,20 @@ function toggleHelp() {
       requestStatus();
     }
   });
+
+  function setData(dataText) {
+    if (!dataView) {
+      dataView = document.getElementById("data");
+    }
+    dataView.textContent = dataText;
+  }
+
+  function setStatus(statusText) {
+    if (!statusView) {
+      statusView = document.getElementById("status");
+    }
+    statusView.textContent = statusText;
+  }
 
   /**
    * Connects to a host and port
@@ -112,26 +123,26 @@ function toggleHelp() {
   function connect(host, port) {
     tcpClient = new TcpClient(host, port);
     tcpClient.connect(function () {
-      term.output('Connected to ' + host + ':' + port + '<br/>');
+      setStatus("Connected to " + host + ":" + port);
       tcpClient.addResponseListener(function (data) {
         if (data[0] != REQUEST_STATUS) {
-          term.output('Invalid response: request code does not match');
+          setData("Invalid response: request code does not match");
           return;
         }
 
         // extract some values
-        var output = '';
+        var output = "";
         var currentTime = new Date();
         output += currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds();
-        output += '<br/>';
-        output += getValue(data, INDEX_TEMP_VORLAUF) + '<br/>';
-        output += getValue(data, INDEX_TEMP_RUECKLAUF) + '<br/>';
-        output += getValue(data, INDEX_TEMP_RUECKLAUF_SOLL) + '<br/>';
-        output += getValue(data, INDEX_TEMP_OUTDOORS) + '<br/>';
-        output += getValue(data, INDEX_TIME_HEIZUNG_RUECKLAUF_MEHR) + '<br/>';
-        output += getValue(data, INDEX_TIME_HEIZUNG_RUECKLAUF_WENIGER) + '<br/>';
-        output += getValue(data, INDEX_TIME_VERDICHTER_STAND) + '<br/>';
-        term.output(output + '<br/>');
+        output += "<br/>";
+        output += getValue(data, INDEX_TEMP_VORLAUF) + "<br/>";
+        output += getValue(data, INDEX_TEMP_RUECKLAUF) + "<br/>";
+        output += getValue(data, INDEX_TEMP_RUECKLAUF_SOLL) + "<br/>";
+        output += getValue(data, INDEX_TEMP_OUTDOORS) + "<br/>";
+        output += getValue(data, INDEX_TIME_HEIZUNG_RUECKLAUF_MEHR) + "<br/>";
+        output += getValue(data, INDEX_TIME_HEIZUNG_RUECKLAUF_WENIGER) + "<br/>";
+        output += getValue(data, INDEX_TIME_VERDICHTER_STAND) + "<br/>";
+        setData(output);
 
         if (isRequestStatus) {
           window.setTimeout(function () {
@@ -154,51 +165,51 @@ function toggleHelp() {
       case INDEX_TIME_VERDICHTER_STAND:
         return getTimeValue(array, index);
     }
-    return 'n/a';
+    return "n/a";
   }
 
   function getTemperatureValue(array, index) {
-    var label = 'Unknown';
+    var label = "Unknown";
     switch (index) {
       case INDEX_TEMP_VORLAUF:
-        label = 'Temp. Vorlauf';
+        label = "Temp. Vorlauf";
         break;
       case INDEX_TEMP_RUECKLAUF:
-        label = 'Temp. Rücklauf';
+        label = "Temp. Rücklauf";
         break;
       case INDEX_TEMP_RUECKLAUF_SOLL:
-        label = 'Temp. Rücklauf Soll';
+        label = "Temp. Rücklauf Soll";
         break;
       case INDEX_TEMP_OUTDOORS:
-        label = 'Temp. Aussen';
+        label = "Temp. Aussen";
         break;
     }
     // offset by 3 (exclude request code, status code and length field)
-    return label + ': ' + (array[index + 3] / 10) + ' ' + UNIT_TEMPERATURE_CELSIUS;
+    return label + ": " + (array[index + 3] / 10) + " " + UNIT_TEMPERATURE_CELSIUS;
   }
 
   function getTimeValue(array, index) {
-    var label = 'Unknown';
+    var label = "Unknown";
     switch (index) {
       case INDEX_TIME_HEIZUNG_RUECKLAUF_MEHR:
-        label = 'T Rücklauf < (T Rücklauf Soll - Hysterese)';
+        label = "T Rücklauf < (T Rücklauf Soll - Hysterese)";
         break;
       case INDEX_TIME_HEIZUNG_RUECKLAUF_WENIGER:
-        label = 'T Rücklauf > (T Rücklauf Soll + Hysterese)';
+        label = "T Rücklauf > (T Rücklauf Soll + Hysterese)";
         break;
       case INDEX_TIME_VERDICHTER_STAND:
-        label = 'Verdichter Stillstand';
+        label = "Verdichter Stillstand";
         break;
     }
     var seconds = array[index + 3];
     var minutes = (seconds - (seconds % 60)) / 60;
     // offset by 3 (exclude request code, status code and length field)
-    return label + ': ' + minutes + ' min ' + (seconds % 60) + ' sec';
+    return label + ": " + minutes + " min " + (seconds % 60) + " sec";
   }
 
   function disconnect() {
     if (tcpClient) {
-      term.output('Disconnected.');
+      setStatus("Disconnected.");
       tcpClient.disconnect();
     }
   }
