fa145a1305e984865566cbe1be5231dc8c7bf52f
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-05-17 10:23:47 +0200

    Rename terminal to simply main.

--- app/launch.js
+++ app/launch.js
@@ -5,7 +5,7 @@
  * @see http://developer.chrome.com/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('terminal.html', {
+  chrome.app.window.create('main.html', {
   	id: "mainwin",
     bounds: {
       width: 600,
