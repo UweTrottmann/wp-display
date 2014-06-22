92d8a938f6525437de7b3979de52caff928d68e1
Uwe Trottmann <uwe.trottmann@gmail.com>
2014-06-22 10:51:29 +0200

    Add source in/out temp, pump active, rest time.
    - Also update some descriptions for clarity.

--- app/controller.js
+++ app/controller.js
@@ -1,48 +1,31 @@
+/**
+ * Request code to get a status response from controller.
+ * @const
+ */
 var REQUEST_STATUS = 3004;
 
 /**
- * Temperatur Vorlauf, Faktor 10, Celsius.
+ * All following are temparature values, factor 10, Celsius.
  * @const
  * @type {number}
  */
 var INDEX_TEMP_OUTGOING = 10;
-/**
- * Temperatur Rücklauf, Faktor 10, Celsius.
- * @const
- * @type {number}
- */
 var INDEX_TEMP_RETURN = 11;
-/**
- * Temperatur Rücklauf Soll, Faktor 10, Celsius.
- * @const
- * @type {number}
- */
 var INDEX_TEMP_RETURN_SHOULD = 12;
-/**
- * Aussentemperatur, Faktor 10, Celsius.
- * @const
- * @type {number}
- */
 var INDEX_TEMP_OUTDOORS = 15;
-/**
- * Temperature Water, Factor 10, Celsius.
- * @const
- * @type {number}
- */
 var INDEX_TEMP_WATER = 17;
-/**
- * Temperature Water Should, Factor 10, Celsius.
- * @const
- * @type {number}
- */
 var INDEX_TEMP_WATER_SHOULD = 18;
+var INDEX_TEMP_SOURCE_IN = 19;
+var INDEX_TEMP_SOURCE_OUT = 20;
 
-/** @const */
+/** 
+ * All following are time values, Seconds.
+ * @const
+ */
+var INDEX_TIME_PUMP_ACTIVE = 67;
+var INDEX_TIME_REST = 71;
 var INDEX_TIME_COMPRESSOR_NOOP = 73;
-
-/** @const */
 var INDEX_TIME_RETURN_LOWER = 74;
-/** @const */
 var INDEX_TIME_RETURN_HIGHER = 75;
 
 function HeatingDisplayControl($scope) {
@@ -52,14 +35,20 @@ function HeatingDisplayControl($scope) {
   $scope.strLabelHost = chrome.i18n.getMessage("hostname");
   $scope.strLabelPort = chrome.i18n.getMessage("port");
   $scope.strTitleAbout = chrome.i18n.getMessage("titleAbout");
+  
   $scope.strLabelTempOutgoing = chrome.i18n.getMessage("tempOutgoing");
   $scope.strLabelTempReturn = chrome.i18n.getMessage("tempReturn");
   $scope.strLabelTempReturnShould = chrome.i18n.getMessage("tempReturnShould");
   $scope.strLabelTempOutdoors = chrome.i18n.getMessage("tempOutdoors");
   $scope.strLabelTempWater = chrome.i18n.getMessage("tempWater");
   $scope.strLabelTempWaterShould = chrome.i18n.getMessage("tempWaterShould");
+  $scope.strLabelTempSourceIn = chrome.i18n.getMessage("tempSourceIn");
+  $scope.strLabelTempSourceOut = chrome.i18n.getMessage("tempSourceOut");
+
   $scope.strLabelTimeReceived = chrome.i18n.getMessage("timeReceived");
   $scope.strLabelTimeCompressorNoop = chrome.i18n.getMessage("timeCompressorNoop");
+  $scope.strLabelTimePumpActive = chrome.i18n.getMessage("timePumpActive");
+  $scope.strLabelTimeRest = chrome.i18n.getMessage("timeRest");
   $scope.strLabelTimeReturnLower = chrome.i18n.getMessage("timeReturnLower");
   $scope.strLabelTimeReturnHigher = chrome.i18n.getMessage("timeReturnHigher");
 
@@ -73,14 +62,19 @@ function HeatingDisplayControl($scope) {
 
   $scope.timeReceived = "00:00:00";
   $scope.timeCompressorNoop = "0 h 0 min 0 sec";
+  $scope.timePumpActive = "0 h 0 min 0 sec";
+  $scope.timeRest = "0 h 0 min 0 sec";
   $scope.timeReturnLower = "0 h 0 min 0 sec";
   $scope.timeReturnHigher = "0 h 0 min 0 sec";
+
   $scope.tempOutdoors = 0.0;
   $scope.tempOutgoing = 0.0;
   $scope.tempReturn = 0.0;
   $scope.tempReturnShould = 0.0;
   $scope.tempWater = 0.0;
   $scope.tempWaterShould = 0.0;
+  $scope.tempSourceIn = 0.0;
+  $scope.tempSourceOut = 0.0;
 
   $scope.textStatus = chrome.i18n.getMessage("disconnected");
   $scope.textBtnConnect = chrome.i18n.getMessage("connect");
@@ -144,6 +138,8 @@ function HeatingDisplayControl($scope) {
         
         // time values
         $scope.timeCompressorNoop = $scope.getValue(data, INDEX_TIME_COMPRESSOR_NOOP);
+        $scope.timePumpActive = $scope.getValue(data, INDEX_TIME_PUMP_ACTIVE);
+        $scope.timeRest = $scope.getValue(data, INDEX_TIME_REST);
         $scope.timeReturnLower = $scope.getValue(data, INDEX_TIME_RETURN_LOWER);
         $scope.timeReturnHigher = $scope.getValue(data, INDEX_TIME_RETURN_HIGHER);
 
@@ -154,6 +150,8 @@ function HeatingDisplayControl($scope) {
         $scope.tempReturnShould = $scope.getValue(data, INDEX_TEMP_RETURN_SHOULD);
         $scope.tempWater = $scope.getValue(data, INDEX_TEMP_WATER);
         $scope.tempWaterShould = $scope.getValue(data, INDEX_TEMP_WATER_SHOULD);
+        $scope.tempSourceIn = $scope.getValue(data, INDEX_TEMP_SOURCE_IN);
+        $scope.tempSourceOut = $scope.getValue(data, INDEX_TEMP_SOURCE_OUT);
 
         // force angular to update watched values
         $scope.$apply();
@@ -204,10 +202,14 @@ function HeatingDisplayControl($scope) {
       case INDEX_TEMP_OUTDOORS:
       case INDEX_TEMP_WATER:
       case INDEX_TEMP_WATER_SHOULD:
+      case INDEX_TEMP_SOURCE_IN:
+      case INDEX_TEMP_SOURCE_OUT:
         return $scope.getTemperatureValue(array, index);
       case INDEX_TIME_RETURN_HIGHER:
       case INDEX_TIME_RETURN_LOWER:
       case INDEX_TIME_COMPRESSOR_NOOP:
+      case INDEX_TIME_PUMP_ACTIVE:
+      case INDEX_TIME_REST:
         return $scope.getTimeValue(array, index);
     }
     return "n/a";
